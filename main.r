a67b83fd4aa10dace9b74ee16e759dbfa9d572fb
JBRS307 <kuba.rekas.307@gmail.com>
2024-01-24 17:48:36 +0100

    initial commit

---
+++ main.r
@@ -0,0 +1,21 @@
+library(stats)
+library(smoof)
+library(rlist)
+source("prs.r")
+source("ms.r")
+
+
+# results <- replicate(50, prs(makeAckleyFunction(20), generatePoints(14, 20, -32.768, 32.768)))
+# 
+# print("PRS, 20 wymiarów:")
+# print(mean(results))
+
+
+msRes <- replicate(50, calcMS(100, 2, -32.768, 32.768, makeAckleyFunction(2)))
+
+value <- mean(msRes[1,])
+budget <- round(mean(msRes[2,]))
+
+print(value)
+print(budget)
+
